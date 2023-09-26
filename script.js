b88dc2b4b30b57bbc3b3945a2d15005b7488d956
joaoemanuels <jemanuel.pi@gmail.com>
2023-09-26 12:28:55 -0300

    Colors-Game

---
+++ script.js
@@ -0,0 +1,38 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+$(".block").click(function () {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  $(document).on('keydown', function (e) {
+    alert('Uma tecla foi pressionada. Código da tecla: ' + (e.which || e.keyCode));
+  });
+});
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+
+function animatePress(currentColor) {
+
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
